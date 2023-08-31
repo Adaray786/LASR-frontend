51c58271a801362aad9f942256a9be82686ae126
robbiecrabtree <robbie.crabtree@kainos.com>
2023-08-31 11:39:18 +0100

    Added user login and

---
+++ src/controller/authController.ts
@@ -0,0 +1,24 @@
+import {Request, Response, Application, response} from "express";
+import { Login } from "../model/auth";
+const authService = require("../service/authService");
+
+module.exports = function(app: Application)
+{
+    app.get('/login', async (req:Request, res:Response) => {
+        res.render("login")
+    })
+    app.post('/login', async (req:Request, res:Response) => {
+        let data: Login = req.body;
+
+        try{
+            req.session.token = await authService.login(data)
+            res.redirect('/products') // front page url
+        }
+        catch(e){
+            console.error(e)
+            res.locals.errormessage = (e as Error).message
+            res.render('login', req.body)
+        }
+
+    })
+}
