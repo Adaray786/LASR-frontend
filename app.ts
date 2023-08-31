d599fdcb3e57cbd4d1f54e58daae006d1b7bc789
Adam Uddin <adamuddin@outlook.com>
2023-08-31 09:53:41 +0100

    fixing dependencies and adding app.ts

---
+++ app.ts
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+const appViews = path.join(__dirname, '/views/');
+
+const nunjucksConfig = {
+    autoescape: true,
+    noCache: true,
+    express: app
+};
+
+nunjucks.configure(appViews, nunjucksConfig);
+
+app.set('view engine', 'html');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, ()=> {
+    console.log("Server listening on port 3000");
+});
